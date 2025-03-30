87141b24c098e15e7e30d426b93938e85c058b8d
xixvivji <119652360+xixvivji@users.noreply.github.com>
2025-03-30 21:56:26 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 10.63 ms, Memory: 91.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String s) {
+        String[] numbers = s.split(" "); 
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        
+        for (String num : numbers) {
+            int value = Integer.parseInt(num); 
+            min = Math.min(min, value);
+            max = Math.max(max, value);
+        }
+        
+        return min + " " + max;
+    }
+}
