aee5ce55cd81dc03ff3a4d67d1765e8b2eb7b569
xixvivji <kjw3568@naver.com>
2025-04-01 23:04:12 +0900

    n진수로 변환후 x의 개수를 세는 방법 추가

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -10,3 +10,19 @@ class Solution {
         return next;
     }
 }
+
+// -> 숫자 ***을 n진수로 변환후 x의 개수를 세는 방법
+//int n = 78;
+//String base3 = Integer.toString(n, 3); // 78을 3진수 문자열로 변환
+//long countOnes = base3.chars().filter(c -> c == '1').count(); // '1' 개수 세기
+
+//public static int countDigitInBase(int n, int base, int digit) {
+//    int count = 0;
+//    while (n > 0) {
+//        if (n % base == digit) {
+//            count++;
+//        }
+//        n /= base;
+//    }
+//    return count;
+//}
