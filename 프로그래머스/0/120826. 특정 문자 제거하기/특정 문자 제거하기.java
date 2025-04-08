41cee48404545d47609273d070cd0cf332429174
xixvivji <kjw3568@naver.com>
2025-04-08 23:28:37 +0900

    stream / stringBuilder

--- 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.java
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.java
@@ -3,3 +3,29 @@ class Solution {
         return my_string.replace(letter, "");
     }
 }
+/*
+import java.util.stream.Collectors;
+
+class Solution {
+    public String solution(String my_string, String letter) {
+        return my_string.chars()  // IntStream
+                .mapToObj(c -> String.valueOf((char) c))
+                .filter(s -> !s.equals(letter))
+                .collect(Collectors.joining());
+    }
+}
+ */
+
+/*
+class Solution {
+    public String solution(String my_string, String letter) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < my_string.length(); i++) {
+            if (my_string.charAt(i) != letter.charAt(0)) {
+                sb.append(my_string.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+}
+ */
