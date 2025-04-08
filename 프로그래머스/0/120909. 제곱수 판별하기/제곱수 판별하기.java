ae88a599bf814a070788b29919d4e8cc018e1fcd
xixvivji <kjw3568@naver.com>
2025-04-08 23:44:18 +0900

    for반복문

--- 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -1,10 +1,23 @@
 class Solution {
     public int solution(int n) {
-        int sqrt = (int)Math.sqrt(n);  
+        int sqrt = (int) Math.sqrt(n);
         if (sqrt * sqrt == n) {
-            return 1;  
+            return 1;
         } else {
-            return 2;  
+            return 2;
         }
     }
 }
+/*
+
+class Solution {
+    public int solution(int n) {
+        for (int i = 1; ; i++) {
+            int square = i * i;
+            if (square == n) return 1;
+            if (square > n) break;
+        }
+        return 2;
+    }
+}
+ */
