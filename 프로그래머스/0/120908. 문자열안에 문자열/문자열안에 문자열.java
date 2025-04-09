ef3fde428aeaa6aa0beac6dc0f8cf7c74f5e0e13
xixvivji <kjw3568@naver.com>
2025-04-09 23:01:11 +0900

    for / indexof

--- 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.java
+++ 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.java
@@ -7,3 +7,37 @@ class Solution {
         }
     }
 }
+/*
+class Solution {
+    public int solution(String str1, String str2) {
+        if (str1.indexOf(str2) != -1) {
+            return 1;
+        } else {
+            return 2;
+        }
+    }
+}
+
+ */
+
+/*
+class Solution {
+    public int solution(String str1, String str2) {
+        int len1 = str1.length();
+        int len2 = str2.length();
+
+        for (int i = 0; i <= len1 - len2; i++) {
+            boolean match = true;
+            for (int j = 0; j < len2; j++) {
+                if (str1.charAt(i + j) != str2.charAt(j)) {
+                    match = false;
+                    break;
+                }
+            }
+            if (match) return 1;
+        }
+
+        return 2;
+    }
+}
+ */
