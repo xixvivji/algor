6b35c4a91243a192a4938a4ad0a937fce7210192
xixvivji <kjw3568@naver.com>
2025-04-09 22:40:58 +0900

    for사용

--- 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.java
+++ 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.java
@@ -3,3 +3,15 @@ class Solution {
         return new StringBuilder(my_string).reverse().toString();
     }
 }
+
+/*
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        for (int i = my_string.length() - 1; i >= 0; i--) {
+            answer += my_string.charAt(i);
+        }
+        return answer;
+    }
+}
+ */
