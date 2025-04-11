5d180f57e2e61687c309c9a5410fe368b3a13bd7
xixvivji <kjw3568@naver.com>
2025-04-11 21:14:14 +0900

    for - char

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -1,17 +1,36 @@
 class Solution {
     public String solution(String my_string) {
         return my_string.chars()
-            .mapToObj(c -> {
-                char ch = (char) c;
-                if (Character.isUpperCase(ch)) {
-                    return Character.toLowerCase(ch);
-                } else {
-                    return Character.toUpperCase(ch);
-                }
-            })
-            .collect(StringBuilder::new,
-                     StringBuilder::append,
-                     StringBuilder::append)
-            .toString();
+                .mapToObj(c -> {
+                    char ch = (char) c;
+                    if (Character.isUpperCase(ch)) {
+                        return Character.toLowerCase(ch);
+                    } else {
+                        return Character.toUpperCase(ch);
+                    }
+                })
+                .collect(StringBuilder::new,
+                        StringBuilder::append,
+                        StringBuilder::append)
+                .toString();
     }
 }
+
+/*
+class Solution {
+    public String solution(String my_string) {
+        StringBuilder sb = new StringBuilder();
+
+        for (char ch : my_string.toCharArray()) {
+            if (Character.isUpperCase(ch)) {
+                sb.append(Character.toLowerCase(ch));
+            } else {
+                sb.append(Character.toUpperCase(ch));
+            }
+        }
+
+        return sb.toString();
+    }
+}
+
+ */
