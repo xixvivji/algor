35cbaa7ffe0332556ff20b21943dbbf012fba306
xixvivji <kjw3568@naver.com>
2025-04-13 00:21:40 +0900

    stream사용

--- 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.java
+++ 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.java
@@ -12,3 +12,16 @@ class Solution {
         return sb.toString();
     }
 }
+
+/*
+
+import java.util.stream.Collectors;
+
+class Solution {
+    public String solution(String my_string, int n) {
+        return my_string.chars()
+                .mapToObj(c -> String.valueOf((char) c).repeat(n))
+                .collect(Collectors.joining());
+    }
+}
+ */
