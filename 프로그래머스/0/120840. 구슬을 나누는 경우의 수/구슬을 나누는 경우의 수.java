2a3f73d1c2577e6326560047d83d5388af208de5
xixvivji <kjw3568@naver.com>
2025-04-15 00:55:53 +0900

    재귀로 팩토리얼

--- 프로그래머스/0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.java
+++ 프로그래머스/0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.java
@@ -12,3 +12,22 @@ class Solution {
         return result;
     }
 }
+/*
+import java.math.BigInteger;
+
+class Solution {
+    public int solution(int balls, int share) {
+        return combination(balls, share).intValue();
+    }
+
+    private BigInteger combination(int n, int r) {
+        return factorial(n).divide(factorial(r).multiply(factorial(n - r)));
+    }
+
+    // 재귀로 팩토리얼 구현
+    private BigInteger factorial(int num) {
+        if (num <= 1) return BigInteger.ONE;
+        return BigInteger.valueOf(num).multiply(factorial(num - 1));
+    }
+}
+ */
