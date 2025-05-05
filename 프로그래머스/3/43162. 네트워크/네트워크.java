00fed386d50f9bfee48e55db47e26f37a0a2b14d
xixvivji <kjw3568@naver.com>
2025-05-05 23:12:40 +0900

    DFS / BFS

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,3 +1,4 @@
+// DPS 방식
 class Solution {
     public int solution(int n, int[][] computers) {
         boolean[] visited = new boolean[n];
@@ -22,3 +23,40 @@ class Solution {
         }
     }
 }
+
+/*  BFS 방식
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int[][] computers) {
+        boolean[] visited = new boolean[n];
+        int answer = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                bfs(i, computers, visited);
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+
+    private void bfs(int start, int[][] computers, boolean[] visited) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(start);
+        visited[start] = true;
+
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            for (int i = 0; i < computers.length; i++) {
+                if (computers[node][i] == 1 && !visited[i]) {
+                    visited[i] = true;
+                    queue.add(i);
+                }
+            }
+        }
+    }
+}
+
+ */
