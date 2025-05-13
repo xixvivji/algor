0d959f24f910607b597ad8c0f8467af1a01622a8
xixvivji <kjw3568@naver.com>
2025-05-13 23:51:08 +0900

    greedy + 정렬 / priority queue

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -26,3 +26,38 @@ class Solution { // greedy + 정렬
         return kind;
     }
 }
+
+/*
+import java.util.*;
+
+class Solution { // priority queue 사용
+    public int solution(int k, int[] tangerine) {
+        Map<Integer, Integer> countMap = new HashMap<>();
+        for (int t : tangerine) {
+            countMap.put(t, countMap.getOrDefault(t, 0) + 1);
+        }
+
+        // 우선순위 큐를 사용하여 개수 기준으로 정렬
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        pq.addAll(countMap.values());
+
+        int total = 0;
+        int kinds = 0;
+
+        while (total < k && !pq.isEmpty()) {
+            total += pq.poll();
+            kinds++;
+        }
+
+        return kinds;
+    }
+}
+
+방식	시간 복잡도	특징	일반적인 속도
+Map + 정렬	O(N + M log M)
+※ N은 tangerine 길이, M은 종류 수	정렬 연산 추가	대부분의 경우 더 빠름
+PriorityQueue	O(N + M log M)
+우선순위 큐 삽입과 제거 포함	코드 간결, 직관적	약간 느릴 수 있음 (힙 연산 overhead)
+
+
+ */
