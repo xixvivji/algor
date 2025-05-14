6555ae7ae9098d059a6cfac6768d673fc37e1baf
xixvivji <kjw3568@naver.com>
2025-05-14 23:55:23 +0900

    .

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,7 +1,7 @@
 class Solution {
     boolean solution(String s) {
-        int count = 0;
-        
+        int count = 0; // '('를 +1, ')'를 -1
+
         for (char c : s.toCharArray()) {
             if (c == '(') {
                 count++;
@@ -9,13 +9,13 @@ class Solution {
                 count--;
             }
 
- 
+            // ')'가 먼저 나오면 잘못된 괄호
             if (count < 0) {
                 return false;
             }
         }
 
-
+        // 모든 '('가 닫혔는지 확인
         return count == 0;
     }
 }
