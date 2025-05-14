a6c36a7c514d2ec79058ad5d053ea07b79106493
xixvivji <kjw3568@naver.com>
2025-05-14 23:56:15 +0900

    카운팅 / 스택

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -19,3 +19,29 @@ class Solution {
         return count == 0;
     }
 }
+
+/*
+import java.util.Stack;
+스택사용
+class Solution {
+    boolean solution(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(c);
+            } else {
+                // 닫는 괄호가 나왔는데 스택이 비어있으면 잘못된 괄호
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                stack.pop(); // 짝이 맞는 여는 괄호 제거
+            }
+        }
+
+        // 스택이 비어있으면 모든 괄호가 잘 짝지어진 것
+        return stack.isEmpty();
+    }
+}
+
+ */
