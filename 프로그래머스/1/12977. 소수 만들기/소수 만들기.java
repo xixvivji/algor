e25258472dcc5a003b0c732b4e82db32d78a280c
xixvivji <kjw3568@naver.com>
2025-05-18 22:07:14 +0900

    재귀

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -2,7 +2,7 @@ class Solution {
     public int solution(int[] nums) {
         int answer = 0;
 
-     
+
         for (int i = 0; i < nums.length - 2; i++) {
             for (int j = i + 1; j < nums.length - 1; j++) {
                 for (int k = j + 1; k < nums.length; k++) {
@@ -18,6 +18,46 @@ class Solution {
     }
 
     private boolean isPrime(int num) {
+        if (num < 2) {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+/*
+
+class Solution {
+    int answer = 0;
+
+    public int solution(int[] nums) {
+        dfs(nums, 0, 0, 0);
+        return answer;
+    }
+
+    // DFS로 3개의 숫자 조합
+    private void dfs(int[] nums, int index, int count, int sum) {
+        if (count == 3) {
+            if (isPrime(sum)) {
+                answer++;
+            }
+            return;
+        }
+
+        for (int i = index; i < nums.length; i++) {
+            dfs(nums, i + 1, count + 1, sum + nums[i]);
+        }
+    }
+
+    // 소수 판별 함수
+    private boolean isPrime(int num) {
         if (num < 2) return false;
 
         for (int i = 2; i <= Math.sqrt(num); i++) {
@@ -27,3 +67,5 @@ class Solution {
         return true;
     }
 }
+
+ */
