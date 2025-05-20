1df94deba4d05dac68cc852c06da3dde4e1c2bf3
xixvivji <kjw3568@naver.com>
2025-05-20 23:50:54 +0900

    2중배열 추가

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -18,3 +18,28 @@ class Solution {
         return answer;
     }
 }
+
+/*
+class Solution {
+    public int[] solution(String s) {
+        int[] answer = new int[s.length()];
+
+        for (int i = 0; i < s.length(); i++) {
+            boolean found = false;
+            for (int j = i - 1; j >= 0; j--) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    answer[i] = i - j;
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                answer[i] = -1;
+            }
+        }
+
+        return answer;
+    }
+}
+
+ */
