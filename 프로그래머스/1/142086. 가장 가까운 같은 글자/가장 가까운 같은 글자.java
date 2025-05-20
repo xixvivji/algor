19ec4848acfff4ae042f8df6133e387e120351b1
xixvivji <kjw3568@naver.com>
2025-05-20 23:57:05 +0900

    방식 다양화

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -43,3 +43,57 @@ class Solution {
 }
 
  */
+/*
+import java.util.*;
+
+class Solution {
+    public List<Integer> solution(String s) {
+        List<Integer> answer = new ArrayList<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char now = s.charAt(i);
+            int distance = -1;
+            for (int j = i - 1; j >= 0; j--) {
+                if (s.charAt(j) == now) {
+                    distance = i - j;
+                    break;
+                }
+            }
+            answer.add(distance);
+        }
+
+        return answer;
+    }
+}
+
+ */
+/*
+import java.util.*;
+
+class Solution {
+    public List<Integer> solution(String s) {
+        List<Integer> answer = new ArrayList<>();
+        Map<Character, Integer> lastIndexMap = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (lastIndexMap.containsKey(ch)) {
+                answer.add(i - lastIndexMap.get(ch));
+            } else {
+                answer.add(-1);
+            }
+            lastIndexMap.put(ch, i);
+        }
+
+        return answer;
+    }
+}
+
+ */
+/*
+방식 번호	자료구조	핵심 로직	시간복잡도	장점
+1	int[]	이중 for문	O(n²)	단순하고 직관적
+2	int[]	HashMap	O(n)	빠름, 최적화된 성능
+3	List<>	이중 for문	O(n²)	동적 리스트 사용 가능
+4	List<>	HashMap	O(n)	빠르고 유연한 리턴 타입
+ */
