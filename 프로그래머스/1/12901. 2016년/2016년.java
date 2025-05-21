cf97178d9afb0d5b475dbf6f764c8f56bd390e42
xixvivji <kjw3568@naver.com>
2025-05-21 23:15:39 +0900

    자바 함수사용

--- 프로그래머스/1/12901. 2016년/2016년.java
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -2,12 +2,12 @@ import java.time.LocalDate;
 import java.time.format.TextStyle;
 import java.util.Locale;
 
-class Solution {
+class Solution { // 자바 함수사용
     public String solution(int a, int b) {
-        
+
         LocalDate date = LocalDate.of(2016, a, b);
-        
-       
+
+
         return date.getDayOfWeek().getDisplayName(TextStyle.SHORT, Locale.ENGLISH).toUpperCase();
     }
 }
