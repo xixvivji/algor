815d5e2090b9a3943fbbea3bc9d0d3a406d3fd7d
xixvivji <119652360+xixvivji@users.noreply.github.com>
2025-08-02 15:16:07 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 92 ms, Memory: 12824 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -5,7 +5,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String word = sc.nextLine();
-        sc.close();
 
         String[] cr = { "c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z=" };
 
