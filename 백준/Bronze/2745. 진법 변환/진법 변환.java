aed9c0ea14a02aa5ad5522fae862f8b5518c85bb
xixvivji <119652360+xixvivji@users.noreply.github.com>
2025-08-03 19:16:14 +0900

    [Bronze II] Title: 진법 변환, Time: 92 ms, Memory: 12948 KB -BaekjoonHub

---
+++ 백준/Bronze/2745. 진법 변환/진법 변환.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        String n = sc.next();
+        int b = sc.nextInt();
+        int tmp = 1;
+        int result = 0;
+        for (int i = n.length() - 1; i >= 0; i--) {
+            char c = n.charAt(i);
+            if (c >= 'A' && c <= 'Z') {
+                result += (c - 55) * tmp;
+            } else {
+                result += (c - 48) * tmp;
+            }
+            tmp *= b;
+        }
+        System.out.println(result);
+
+        sc.close();
+    }
+}
