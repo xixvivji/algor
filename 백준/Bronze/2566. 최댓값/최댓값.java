f92f67cb0ee7d49c40c7f5935b29e60df2e164c3
xixvivji <119652360+xixvivji@users.noreply.github.com>
2025-08-03 14:24:06 +0900

    Update 최댓값.java

--- 백준/Bronze/2566. 최댓값/최댓값.java
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -28,4 +28,37 @@ public class Main {
         }
 
     }
+
 }
+
+
+/*
+package javaStudy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        int[][] arr = new int[9][9];
+        int max = Integer.MIN_VALUE;
+        int row = 0, col = 0;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        for (int i = 0; i < 9; i++) {
+            String[] tokens = br.readLine().split(" ");
+            for (int j = 0; j < 9; j++) {
+                arr[i][j] = Integer.parseInt(tokens[j]);
+                if (arr[i][j] > max) {
+                    max = arr[i][j];
+                    row = i;
+                    col = j;
+                }
+            }
+        }
+        System.out.println(max);
+        System.out.println((row + 1) + " " + (col + 1));
+    }
+}
+*/
