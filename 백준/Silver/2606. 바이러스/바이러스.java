3c1c038b98b7269cecd92a253818fb17889a29d7
xixvivji <119652360+xixvivji@users.noreply.github.com>
2025-08-05 19:54:15 +0900

    [Silver III] Title: 바이러스, Time: 68 ms, Memory: 11676 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static int node;
+    static ArrayList<Integer>[] graph;
+    static boolean[] visited;
+    static int count = 0;
+    public static void main(String[] args) throws IOException,NoSuchElementException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        node = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        graph = new ArrayList[N+1];
+        visited =new boolean[N+1];
+        for (int i = 0; i <=N; i++) {
+            graph[i] = new ArrayList<>();
+            
+        }
+
+        for(int i = 0; i < node; i++ ){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            graph[a].add(b);
+            graph[b].add(a);
+
+        }
+        dfs(1);
+        System.out.println(count);
+    }
+
+ private static void dfs(int n) {
+        visited[n] = true;
+        for (int i : graph[n]) {
+            if (!visited[i]) {
+                count++;
+                dfs(i);
+            }
+        }
+    }
+}
