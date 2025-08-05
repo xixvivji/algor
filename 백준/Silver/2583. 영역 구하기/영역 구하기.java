655a551f8089641726df49c83a69076d037162b7
xixvivji <119652360+xixvivji@users.noreply.github.com>
2025-08-05 22:14:01 +0900

    [Silver I] Title: 영역 구하기, Time: 88 ms, Memory: 12720 KB -BaekjoonHub

---
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.java
@@ -0,0 +1,65 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int M, N, K;
+    static int[][] map;
+    static boolean[][] visited;
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
+
+    static int dfs(int x, int y) {
+        visited[x][y] = true;
+        int area = 1;
+        for (int d = 0; d < 4; d++) {
+            int nx = x + dx[d];
+            int ny = y + dy[d];
+            if (nx >= 0 && nx < M && ny >= 0 && ny < N && !visited[nx][ny] && map[nx][ny] == 0) {
+                area += dfs(nx, ny);
+            }
+        }
+        return area;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        map = new int[M][N];
+        visited = new boolean[M][N];
+        for (int k = 0; k < K; k++) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+            for (int i = y1; i < y2; i++) {
+                for (int j = x1; j < x2; j++) {
+                    map[i][j] = 1;
+                }
+            }
+        }
+
+        List<Integer> areas = new ArrayList<>();
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+                if (map[i][j] == 0 && !visited[i][j]) {
+                    areas.add(dfs(i, j));
+                }
+            }
+        }
+        Collections.sort(areas);
+        System.out.println(areas.size());
+        for (int area : areas) {
+            System.out.print(area + " ");
+        }
+    }
+}
