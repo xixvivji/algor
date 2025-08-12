bf31fc48c520b546269a9b3594fcf023497b1e35
xixvivji <119652360+xixvivji@users.noreply.github.com>
2025-08-12 23:10:56 +0900

    [Silver III] Title: 프린터 큐, Time: 100 ms, Memory: 12368 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.java
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.java
@@ -1,40 +1,41 @@
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Collections;
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine()); 
+        int T = Integer.parseInt(br.readLine());
 
         StringBuilder sb = new StringBuilder();
         while (T-- > 0) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int N = Integer.parseInt(st.nextToken()); 
-            int M = Integer.parseInt(st.nextToken()); 
-
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+            
             Queue<int[]> queue = new LinkedList<>();
+            PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
             st = new StringTokenizer(br.readLine());
             for (int i = 0; i < N; i++) {
-                queue.add(new int[] {i, Integer.parseInt(st.nextToken())});
+                int priority = Integer.parseInt(st.nextToken());
+                queue.add(new int[]{i, priority});
+                pq.add(priority);
             }
 
             int printCount = 0;
             while (!queue.isEmpty()) {
-                int[] currentDoc = queue.poll(); 
-                boolean isPrintable = true;
-
-                for (int[] docInQueue : queue) {
-                    if (docInQueue[1] > currentDoc[1]) {
-                        isPrintable = false;
-                        break;
-                    }
-                }
-
-                if (isPrintable) {
+                int[] currentDoc = queue.poll();
+                
+                if (currentDoc[1] == pq.peek()) {
                     printCount++;
+                    pq.poll();
+                    
                     if (currentDoc[0] == M) {
                         sb.append(printCount).append('\n');
                         break;
