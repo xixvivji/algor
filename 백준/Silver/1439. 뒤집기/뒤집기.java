9e2a6c27835efb1f5b7de3454e031413aeb8703a
xixvivji <119652360+xixvivji@users.noreply.github.com>
2025-08-14 22:12:19 +0900

    [Silver V] Title: 뒤집기, Time: 68 ms, Memory: 11512 KB -BaekjoonHub

---
+++ 백준/Silver/1439. 뒤집기/뒤집기.java
@@ -0,0 +1,35 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+ 
+	public static void main(String[] args) throws IOException {
+ 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str= br.readLine();
+        
+        int count0 = 0;
+        int count1 = 0;
+        
+        if(str.charAt(0)=='0'){
+            count0++;
+        }
+        else{
+            count1++;
+        }
+        for (int i = 1; i < str.length(); i++) {
+            if (str.charAt(i) != str.charAt(i - 1)) {
+                if (str.charAt(i) == '0') {
+                    count0++;
+                } else {
+                    count1++;
+                }
+            }
+        }
+        System.out.println(count0 > count1 ? count1 : count0);
+
+    }
+}
