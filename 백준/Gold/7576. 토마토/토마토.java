ecdd2773c75407d3ecfcb3a7f131b01036be2490
xixvivji <119652360+xixvivji@users.noreply.github.com>
2025-08-24 18:14:20 +0900

    [Gold V] Title: 토마토, Time: 496 ms, Memory: 120512 KB -BaekjoonHub

---
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -0,0 +1,75 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+    static int[][] arr;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static Queue<Node> queue = new LinkedList<>();
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        M = Integer.parseInt(st.nextToken()); 
+        N = Integer.parseInt(st.nextToken()); 
+        arr = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+                if (arr[i][j] == 1) {
+                    queue.add(new Node(i, j));
+                }
+            }
+        }
+
+        bfs();
+
+        int maxDays = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (arr[i][j] == 0) {
+                    System.out.println(-1);
+                    return;
+                }
+                maxDays = Math.max(maxDays, arr[i][j]);
+            }
+        }
+
+        System.out.println(maxDays - 1);
+    }
+
+    static void bfs() {
+        while (!queue.isEmpty()) {
+            Node current = queue.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int nextX = current.x + dx[i];
+                int nextY = current.y + dy[i];
+
+                if (nextX >= 0 && nextY >= 0 && nextX < N && nextY < M && arr[nextX][nextY] == 0) {
+                    arr[nextX][nextY] = arr[current.x][current.y] + 1;
+                    queue.add(new Node(nextX, nextY));
+                }
+            }
+        }
+    }
+
+    static class Node {
+        int x;
+        int y;
+
+        public Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+}
