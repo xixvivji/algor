b9843e229309678195407365120d3ec8a87d65f3
xixvivji <119652360+xixvivji@users.noreply.github.com>
2025-08-27 20:11:12 +0900

    [Gold IV] Title: 동전 1, Time: 72 ms, Memory: 11644 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int n,k;
+    static int [] arr;
+    static int [] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        arr = new int [n+1];
+        dp = new int[k+1];
+        dp[0] = 1;
+   
+        for (int i = 1; i <= n; i++) {
+         arr[i] = Integer.parseInt(br.readLine()); 
+       
+        }
+        
+        
+        for(int i = 1; i<= n; i++) {
+        	for(int j = arr[i]; j <= k; j++)
+        	{
+        		dp[j] += dp[j- arr[i]];
+        	}
+        }
+        
+    
+        System.out.println(dp[k]);
+    }
+    
+    
+
+   
+}
