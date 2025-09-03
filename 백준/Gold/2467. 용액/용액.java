2fee5730909e15fa46bce6ee7c6cd1322f37a4d7
xixvivji <119652360+xixvivji@users.noreply.github.com>
2025-09-03 20:56:39 +0900

    [Gold V] Title: 용액, Time: 248 ms, Memory: 31476 KB -BaekjoonHub

---
+++ 백준/Gold/2467. 용액/용액.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        int [] arr = new int [N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        for(int i = 0; i < N; i++) {
+        arr[i] = Integer.parseInt(st.nextToken());	
+        }
+        Arrays.sort(arr);
+        
+        int start = 0;
+        int end =N-1;
+        int min =Integer.MAX_VALUE;
+        int arr1 = 0;
+        int arr2 = 0;
+        	while(start < end) {
+        		int sum = arr[start] + arr[end];
+        		int abs = Math.abs(sum);
+        		if(abs < min) {
+        			min = abs;
+        		arr1 = arr[start];
+        		arr2 = arr[end];
+        		}
+        		if(sum < 0) start++;
+        		else end--;
+        		
+        	}
+        	System.out.println(arr1 + " " + arr2);
+        
+    }
+}
