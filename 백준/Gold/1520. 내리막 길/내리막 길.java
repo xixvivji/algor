cdb13e167ccc7e095aff1ccf2f6059d02e4f1c5b
xixvivji <119652360+xixvivji@users.noreply.github.com>
2025-09-07 21:59:42 +0900

    [Gold III] Title: 내리막 길, Time: 300 ms, Memory: 39232 KB -BaekjoonHub

---
+++ 백준/Gold/1520. 내리막 길/내리막 길.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M;
+    static int[][] map, dp;
+    static int[] dx = {0, 0, 1, -1};
+    static int[] dy = {1, -1, 0, 0};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        map = new int[N][M];
+        dp = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+                dp[i][j] = -1;
+            }
+        }
+
+        System.out.println(dfs(0, 0));
+    }
+
+    static int dfs(int x, int y) {
+        if (x == N - 1 && y == M - 1) return 1; 
+        if (dp[x][y] != -1) return dp[x][y];    
+
+        dp[x][y] = 0; 
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (nx >= 0 && ny >= 0 && nx < N && ny < M) {
+                if (map[nx][ny] < map[x][y]) {
+                    dp[x][y] += dfs(nx, ny);
+                }
+            }
+        }
+        return dp[x][y];
+    }
+}
