e467faf374dc2cf224ab97e4c6197bdf18b3d593
xixvivji <119652360+xixvivji@users.noreply.github.com>
2025-09-15 22:18:04 +0900

    [D3] Title: 퍼펙트 셔플, Time: 101 ms, Memory: 30,572 KB -BaekjoonHub

---
+++ SWEA/D3/3499. 퍼펙트 셔플/퍼펙트 셔플.java
@@ -0,0 +1,56 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// double b;
+// char g;
+// String var;
+// long AB;
+// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
+// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
+// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
+// var = sc.next();                            // 문자열 1개 입력받는 예제
+// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;                            
+// double b = 1.0;               
+// char g = 'b';
+// String var = "ABCDEFG";
+// long AB = 12345678901234567L;
+//System.out.println(a);                       // int 변수 1개 출력하는 예제
+//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
+//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
+//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
+//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+        for (int tc = 1; tc <= T; tc++) {
+            int N = Integer.parseInt(br.readLine());
+            String[] arr = br.readLine().split(" ");
+
+            int mid = (N + 1) / 2;
+            Queue<String> q1 = new LinkedList<>();
+            Queue<String> q2 = new LinkedList<>();
+            for (int i = 0; i < mid; i++) q1.add(arr[i]);
+            for (int i = mid; i < N; i++) q2.add(arr[i]);
+
+            sb.append("#").append(tc).append(" ");
+            while (!q1.isEmpty() || !q2.isEmpty()) {
+                if (!q1.isEmpty()) sb.append(q1.poll()).append(" ");
+                if (!q2.isEmpty()) sb.append(q2.poll()).append(" ");
+            }
+            sb.append("\n");
+        }
+        System.out.print(sb);
+    }
+}
