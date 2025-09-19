0e8408b25e699933eb89d9d724822817ccdced7f
xixvivji <119652360+xixvivji@users.noreply.github.com>
2025-09-19 21:49:23 +0900

    [Silver V] Title: 타일 장식물, Time: 72 ms, Memory: 11548 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/13301. 타일 장식물/타일 장식물.java
@@ -0,0 +1,19 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        long[] dp = new long[N + 2];
+        dp[1] = 4;
+        dp[2] = 6;
+        for (int i = 3; i <= N; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        System.out.println(dp[N]);
+    }
+}
