ef8abdb2cfcd896134b70910d3b348f75414d6d0
xixvivji <119652360+xixvivji@users.noreply.github.com>
2025-09-20 16:37:22 +0900

    [Silver IV] Title: Generations of Tribbles, Time: 64 ms, Memory: 11372 KB -BaekjoonHub

---
+++ 백준/Silver/9507. Generations of Tribbles/Generations of Tribbles.java
@@ -0,0 +1,25 @@
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        for (int tc = 0; tc < T; tc++) {
+            int n = Integer.parseInt(br.readLine());
+            long dp[] = new long[Math.max(n + 2, 4)];
+            dp[0] = 1;
+            dp[1] = 1;
+            dp[2] = 2;
+            dp[3] = 4;
+            for (int i = 4; i <= n; i++) {
+                dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3] + dp[i - 4];
+            }
+            System.out.println(dp[n]);
+
+        }
+    }
+}
