548898352492a346e1c3c4748f0e0d033336de1a
xixvivji <119652360+xixvivji@users.noreply.github.com>
2025-09-20 23:25:03 +0900

    [Bronze I] Title: 캠핑, Time: 104 ms, Memory: 12648 KB -BaekjoonHub

---
+++ 백준/Bronze/4796. 캠핑/캠핑.java
@@ -0,0 +1,24 @@
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = 1;
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int L = Integer.parseInt(st.nextToken());
+            int P = Integer.parseInt(st.nextToken());
+            int V = Integer.parseInt(st.nextToken());
+            if (L == 0 && P == 0 && V == 0) {
+                break;
+            }
+
+            int result = (V / P) * L + Math.min(V % P, L);
+            System.out.println("Case " + tc + ": " + result);
+            tc++;
+        }
+    }
+}
