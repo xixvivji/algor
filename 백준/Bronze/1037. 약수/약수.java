6d5b59f2fa899b952ebbf21901643866d95aea9e
xixvivji <119652360+xixvivji@users.noreply.github.com>
2025-09-20 23:21:03 +0900

    [Bronze I] Title: 약수, Time: 64 ms, Memory: 11592 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.java
@@ -0,0 +1,24 @@
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        if (N == 1) {
+            System.out.println(arr[0] * arr[0]);
+        } else {
+            System.out.println(arr[N - 1] * arr[0]);
+        }
+    }
+}
