ccdf95b996c56dfcaf68e07d30e1881ce5ed439f
xixvivji <119652360+xixvivji@users.noreply.github.com>
2025-10-24 22:03:57 +0900

    [Silver III] Title: 블로그, Time: 248 ms, Memory: 38272 KB -BaekjoonHub

---
+++ 백준/Silver/21921. 블로그/블로그.java
@@ -0,0 +1,44 @@
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int X = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int sum = 0;
+        for (int i = 0; i < X; i++) {
+            sum += arr[i];
+        }
+        int max = sum;
+        int count = 1;
+
+        for (int i = X; i < N; i++) {
+            sum = sum - arr[i - X] + arr[i];
+            if (sum > max) {
+                max = sum;
+                count = 1;
+            } else if (sum == max) {
+                count++;
+            }
+        }
+
+        if (max == 0) {
+            System.out.println("SAD");
+        } else {
+            System.out.println(max);
+            System.out.println(count);
+        }
+    }
+}
