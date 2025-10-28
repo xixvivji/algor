6a9f29ab1a5c98295eb9c08f51adbbbe452b04fe
xixvivji <119652360+xixvivji@users.noreply.github.com>
2025-10-28 21:14:43 +0900

    [Bronze IV] Title: 세 수, Time: 64 ms, Memory: 11580 KB -BaekjoonHub

---
+++ 백준/Bronze/10817. 세 수/세 수.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int arr[] = new int[3];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < 3; i++) {
+        arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        
+        System.out.println(arr[1]);
+        
+    }
+}
