01320f7f146c9b3fb38476215d04b521d4554feb
xixvivji <119652360+xixvivji@users.noreply.github.com>
2025-10-28 21:25:24 +0900

    [Bronze I] Title: 알고리즘 수업 - 피보나치 수 1, Time: 64 ms, Memory: 11500 KB -BaekjoonHub

---
+++ 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[n + 2];
+        dp[1] = 1;
+        dp[2] = 1;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        int count1 = dp[n];
+        int count2 = n - 2;
+        System.out.println(count1 + " " + count2);
+    }
+}
